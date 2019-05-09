35a1fb2ba07e0fa4470fc6e05ee78fd123ffed7c
Umut Afacan <umut.afacan@commencis.com>
2019-05-09 19:16:40 +0300

    Add initial classes

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Umut Afacan on 09/05/2019.
+//  Copyright © 2019 umutafacan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
